2e7a36135fcb6338abd17ff3bd3c430ff9e71a34
sushmi5 <saisusmithabatchu@gmail.com>
2017-11-23 16:51:39 -0500

    added Display Grid

---
+++ padma/DisplayGridController.java
@@ -0,0 +1,24 @@
+
+package padma;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+
+public class DisplayGridController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    //@FXML GridPane gridContainer;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
